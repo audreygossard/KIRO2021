4c7ade8ea6247d776a44bf435c4f9ed1f59cc9ef
Alice Krychowski <alicekrychowskimac@MacBook-Pro-de-Alice.local>
2021-05-06 13:48:14 +0200

    ajout d'itinéraire

--- train.jl
+++ train.jl
@@ -6,7 +6,18 @@ struct Train
     typeCirculation::String
     dateHeure::String
     typesMateriels::String
+	itineraire::Int
 
     Train(; id, sensDepart, voieEnLigne, voieAQuai, typeCirculation, dateHeure, typesMateriels) =
-        new(id, sensDepart, voieEnLigne, voieAQuai, typeCirculation, dateHeure, typesMateriels)
+        new(id, sensDepart, voieEnLigne, voieAQuai, typeCirculation, dateHeure, typesMateriels, -1)
+end
+
+function train_into_dict(train::Train)::Dict
+	if train.itineraire == -1
+		itineraire = "notAffected"
+	else
+		itineraire = string(train.itineraire)
+	end
+    dico = Dict("voieAQuai" => train.voieAQuai, "itineraire" => itineraire)
+	return dico
 end
