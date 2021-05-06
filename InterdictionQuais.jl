bb962b0872798c3eb93e1f4ff3984d115b83d2c9
Alice Krychowski <alicekrychowskimac@MacBook-Pro-de-Alice.local>
2021-05-06 13:29:06 +0200

    struct InterdictionQuais

---
+++ InterdictionQuais.jl
@@ -0,0 +1,28 @@
+struct InterdictionQuais
+    voiesAQuaiInterdites::Vector{String}
+    voiesEnLigne::Vector{String}
+    typesMateriels::Vector{Int}
+    typesCirculation::Vector{String} # Ou Int ?
+    Itineraire(; voiesAQuaiInterdites, voieEnLigne, typesCirculation, voieAQuai) = new(voiesAQuaiInterdites, voieEnLigne, typesCirculation, voieAQuai)
+end
+
+function Base.show(io::IO, interdic::InterdictionQuais)
+    str = "InterdictionQuais\n"
+    str *= "voiesAQuaiInterdites :\n"
+    for i in 1:length(interdic.voiesAQuaiInterdites)
+        str *= "       $(interdic.voiesAQuaiInterdites[i])\n"
+    end
+    str *= "voiesEnLigne : \n"
+    for i in 1:length(interdic.voiesEnLigne)
+        str *= "       $(interdic.voiesEnLigne[i])\n"
+    end
+    str *= "typesMateriels : \n"
+    for i in 1:length(interdic.typesMateriels)
+        str *= "       $(interdic.typesMateriels[i])\n"
+    end
+    str *= "typesCirculation : \n"
+    for i in 1:length(interdic.typesCirculation)
+        str *= "       $(interdic.typesCirculation[i])\n"
+    end
+    print(io, str)
+end
