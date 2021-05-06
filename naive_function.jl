9f3a3836c94d57e79719e4be946fbb86dad5837f
Dana Zilberberg <danazilberberg@MacBook-Pro-de-Pierre.local>
2021-05-06 14:16:16 +0200

    deubt solution naive

---
+++ naive_function.jl
@@ -0,0 +1,83 @@
+
+#affecte aux trains d'un groupe G le meme quai qt
+function affecte_quai_train(qt, G)
+  #N = nombre de train dans G
+  for i=1:N
+    G[i].voieAQuai = gt
+  end
+  return G
+end
+
+#affecte aux trains d'un groupe G le meme itineraire it
+function affecte_itineraire_train(it, G)
+  #N = nombre de train dans G
+  for i=1:N
+    G[i].itineraire = it
+  end
+  return G
+end
+
+#Gestion contrainte 2
+#on affecte a chaque train d'un groupe le meme quai que le premier train du groupe
+function affecte_quai_train(trains)
+  #NG = nombbre de groupes
+  for i=1:NG #pour  chaque groupe
+    qt = trains[i, 1].voieAQuai
+    trains[i] = affecte_quai_train(qt, trains[i])
+  end
+  return trains
+end
+
+#Gestion contrainte 1
+#pour chaque train d'un groupe, si qt, lt n'est pas un itineraire on met qt = qvide
+#sinon on met le premier itineraire qu'on trouve
+function itineaire_admissible_groupe(G)
+  itineaires_admissibles = True
+  #N = nombre de train dans G
+  #I = nombre d'itineraires
+  for i=1:N
+    bool = False #false si il n y a pas d'itineraire pour le train i
+    for j=1:I
+      if (itineraires[j].sensDepart == G[i].sensDepart && itineraires[j].voieEnLigne == G[i].voieEnLigne && itineraires[j].voieAQuai == G[i].voieAQuai)
+        G[i].itineraire = j
+        bool = True
+        break #on ne cherche plus d'autre itineraire pour ce train
+      end
+    end
+    if not(bool) #on a trouve aucun itineraire
+      itineraires_admissibles = false #on met tout le monde a it nul et qt nul
+      break #on arrete de tout parcourir
+    end
+  end
+  if not(itineaires_admissibles)
+    G = affecte_quai_train("Notaffected", G)
+    G = affecte_itineraire_train("Notaffected", G)
+  end
+  return G
+end
+
+
+function contrainte2(trains)
+  #NG = nombre de groupes
+  for i=1:NG #pour  chaque groupe
+    trains[i] = itineaire_admissible_groupe(trains[i])
+  end
+  return trains
+end
+
+
+function voie_en_ligne(train, f)
+  liste_voies_ligne = interdictionsQuais[f].voiesEnLigne
+  lt = train.voieEnLigne
+  return lt in liste_voies_ligne
+end
+
+function types_Materiels(train, f)
+  Materiel = 
+
+function contrainte3_groupe(G)
+  admissile = true
+  #N = nombre de train dans G
+  #F = nombre d'interdictions
+  for i=1:N
+    for f=1:F
