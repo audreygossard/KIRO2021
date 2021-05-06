56cb46f546c40d6f89cdd10dfcff05dcf45af1e2
audreygossard <>
2021-05-06 14:17:02 +0200

    Update test_solution_naive.jl

--- test_solution_naive.jl
+++ test_solution_naive.jl
@@ -1,15 +1,13 @@
 include("parser.jl")
-include("solution.jl")
-include("naive_function.jl")
+#include("solution.jl")
+#include("naive_function.jl")
 
 
-path = joinpath(".", "instances/instance_S.IN")
-path_write_sol = joinpath(".", "solution_naive/sol_S.txt")
+path = joinpath(".", "instances/A.json")
+#path_write_sol = joinpath(".", "solution_naive/sol_S.txt")
 
-instance = read_instance(path)
-
-
+read_instance(path)
 
 
 
-write_solution(sol, path_write_sol)
+#write_solution(sol, path_write_sol)
